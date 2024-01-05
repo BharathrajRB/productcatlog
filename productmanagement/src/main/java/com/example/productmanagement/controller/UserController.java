544b5ae6de09fcedca15c5aac3cd26d886a0344e
BharathrajRB <bharathrajrb20it@gmail.com>
2024-01-05 15:15:00 +0530

    Changes

--- productmanagement/src/main/java/com/example/productmanagement/controller/UserController.java
+++ productmanagement/src/main/java/com/example/productmanagement/controller/UserController.java
@@ -11,13 +11,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-@RestController
-@RequestMapping("/users")
-public class UserController {
 
+@RestController
+// @RequestMapping("/users")
+public class UserController {
+    @Autowired
     private final UserService userService;
 
-    @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
